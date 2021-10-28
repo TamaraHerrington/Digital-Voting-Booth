90d310d43f70b49ed380e2199c8e9ba5e1656a19
TamaraHerrington <tamaraherrington24@gmail.com>
2021-10-29 00:00:58 +0100

    create candidate constructor and instances of candidate

--- src/main/java/com/VotingBooth/Candidate.java
+++ src/main/java/com/VotingBooth/Candidate.java
@@ -1,5 +1,8 @@
 package com.VotingBooth;
 
+import javax.naming.Name;
+import java.sql.SQLOutput;
+
 public class Candidate {
     private String name;
     private String officeAddress;
@@ -29,6 +32,20 @@ public class Candidate {
     public void setName(String name) {
         this.name = name;
     }
+
+    //create constructor method
+
+    public Candidate(String name, String officeAddress){// Enum partyAffiliation) - why tf does this enum NOT WORK??)
+        this.name = name;
+        this.officeAddress = officeAddress;
+    }
+//instances of the class candidate
+    public static void main(String[]args){
+        Candidate Bruce = new Candidate( "Bruce Chang", "34 Woolworth Road");
+        Candidate Lora = new Candidate("Lora Smith", "7 Freemans Quay");
+        Candidate Cameron = new Candidate("Cameron MacDonald", "2 New Street");
+    }
 }
+
 
 //politicalaffliation as an enum maybe??
