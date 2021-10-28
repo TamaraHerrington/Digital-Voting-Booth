641176a83b22429bbf538fabc3538c6812040039
TamaraHerrington <tamaraherrington24@gmail.com>
2021-10-29 00:05:31 +0100

    create new empty test

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+public class NewTest {
+    //try a basic af method like .toUpperCase or whatever
+   @Test
+    void PracticeTest(){
+       assertThat("actual").isEqualTo("Expected");
+
+       // must include a:
+
+       //given
+
+       //when
+
+       //then
+   }
+}
